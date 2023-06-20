4e6cc97031e42c74c4067ade431926a1bb1c124d
AxVol <ax_vol@mail.ru>
2023-06-20 12:17:39 +0300

    paste page and validation for paste

--- pasteBin/Areas/Home/Controllers/HomeController.cs
+++ pasteBin/Areas/Home/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 using pasteBin.Areas.Home.Models;
 
 namespace pasteBin.Areas.Home.Controllers
@@ -8,10 +9,41 @@ namespace pasteBin.Areas.Home.Controllers
     {
         [Route("")]
         [Route("Home")]
+        [HttpGet]
         public IActionResult Index()
         {
-            PeopleModel people = new PeopleModel("Axvol", 21);
-            return View(people);
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(PasteModel paste)
+        {
+
+        }
+
+        [Route("Paste")]
+        [HttpPost]
+        public IActionResult Article(PasteModel paste)
+        {
+            if (ModelState.IsValid)
+                return View(paste);
+
+            string errorMessage = String.Empty;
+            
+            foreach (var item in ModelState)
+            {
+                if (item.Value.ValidationState == ModelValidationState.Invalid)
+                {
+                    errorMessage += $"{errorMessage} \n Ошибки {item.Key}";
+
+                    foreach (var error in item.Value.Errors)
+                    {
+                        errorMessage += $"{errorMessage} - {error.ErrorMessage}\n";
+                    }
+                }
+            }
+
+            return View(errorMessage);
         }
     }
 }
---
+++ pasteBin/Areas/Home/Models/PasteModel.cs
@@ -0,0 +1,55 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace pasteBin.Areas.Home.Models
+{
+    public class PasteModel
+    {
+        private readonly int offsetDelete = 30;
+        private string description;
+        private DateTime? deleteDate;
+
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Text { get; set; }
+        public string? Author { get; set; }
+        public DateTime CreateAt { get; set; }
+        public DateTime? DeleteDate 
+        {
+            get
+            {
+                return deleteDate;
+            }
+            set
+            {
+                if (value == null || value < DateTime.Now)
+                {
+                    deleteDate = DateTime.Now.AddDays(offsetDelete);
+
+                    return;
+                }
+                
+                deleteDate = value;
+            }
+        }
+        public string? Description
+        {
+            get => description;
+            set
+            {
+                if (value == null)
+                {
+                    description = "Описание отсутствует";
+                    return;
+                }
+
+                description = value;
+            }
+        }
+
+        public PasteModel() 
+        {
+            CreateAt = DateTime.Now;
+        }
+    }
+}
